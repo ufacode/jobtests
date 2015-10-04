d5dd1c1303aeb21562ff9c4d5655cee0b371a217
MpaK <mrak69@gmail.com>
2015-10-04 22:07:40 +0700

    Design theme

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+100.times do
+  Post.create(
+          title: Faker::Lorem.sentence(5),
+          description: Faker::Lorem.sentence(100)
+  )
+end
